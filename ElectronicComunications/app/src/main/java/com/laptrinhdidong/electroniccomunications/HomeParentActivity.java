b956edd4bac09c4dc9cca65c4cbac5498a69050a
vanha1108 <nguyenvanha11899@gmail.com>
2021-06-02 19:21:13 +0700

    Notification of student and parent

--- ElectronicComunications/app/src/main/java/com/laptrinhdidong/electroniccomunications/HomeParentActivity.java
+++ ElectronicComunications/app/src/main/java/com/laptrinhdidong/electroniccomunications/HomeParentActivity.java
@@ -1,14 +1,40 @@
 package com.laptrinhdidong.electroniccomunications;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.laptrinhdidong.electroniccomunications.adapter.NotificationAdapter;
+import com.laptrinhdidong.electroniccomunications.model.Notification;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeParentActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerHomeParent;
+    private List<Notification> notifications = new ArrayList<>();
+    private NotificationAdapter notificationAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_parent);
+
+        recyclerHomeParent = findViewById(R.id.recyclerHomeParent);
+
+        Notification notification = new Notification("Notification 1", "This is content of notification 1");
+        notifications.add(notification);
+        Notification notification2 = new Notification("Notification 2", "This is content of notification 2");
+        notifications.add(notification2);
+        Notification notification3 = new Notification("Notification 3", "This is content of notification 3");
+        notifications.add(notification3);
+
+        notificationAdapter = new NotificationAdapter(notifications, this);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, RecyclerView.VERTICAL, false);
+        recyclerHomeParent.setLayoutManager(linearLayoutManager);
+        recyclerHomeParent.setAdapter(notificationAdapter);
     }
 }
