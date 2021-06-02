f5682d057e1b7802673b4195627e5a6c797c3954
vanha1108 <nguyenvanha11899@gmail.com>
2021-06-02 22:07:31 +0700

    Revert MainActivity

--- ElectronicComunications/app/src/main/java/com/laptrinhdidong/electroniccomunications/fragment/HomeFragment.java
+++ ElectronicComunications/app/src/main/java/com/laptrinhdidong/electroniccomunications/fragment/HomeFragment.java
@@ -26,19 +26,18 @@ public class HomeFragment extends Fragment {
     @NonNull
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @NonNull ViewGroup container, @NonNull Bundle saveInsatnceState) {
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
-        Notification notification = new Notification("Notification 1", "This is content of notification 1");
-        notifications.add(notification);
-        Notification notification2 = new Notification("Notification 2", "This is content of notification 2");
-        notifications.add(notification2);
-        Notification notification3 = new Notification("Notification 3", "This is content of notification 3");
-        notifications.add(notification3);
-
-        recyclerHome = view.findViewById(R.id.recyclerHome);
-        notificationAdapter = new NotificationAdapter(notifications, getContext());
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(), RecyclerView.VERTICAL, false);
-        recyclerHome.setLayoutManager(linearLayoutManager);
-        recyclerHome.setAdapter(notificationAdapter);
-        return view;
+        return inflater.inflate(R.layout.fragment_home, container, false);
+//        Notification notification = new Notification("Notification 1", "This is content of notification 1");
+//        notifications.add(notification);
+//        Notification notification2 = new Notification("Notification 2", "This is content of notification 2");
+//        notifications.add(notification2);
+//        Notification notification3 = new Notification("Notification 3", "This is content of notification 3");
+//        notifications.add(notification3);
+//
+//        recyclerHome = view.findViewById(R.id.recyclerHome);
+//        notificationAdapter = new NotificationAdapter(notifications, getContext());
+//        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(), RecyclerView.VERTICAL, false);
+//        recyclerHome.setLayoutManager(linearLayoutManager);
+//        recyclerHome.setAdapter(notificationAdapter);
     }
 }
